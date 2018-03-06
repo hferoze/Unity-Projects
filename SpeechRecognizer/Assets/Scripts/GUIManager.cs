40d482bc2c92173c74bb0146ba5eb9a85e1de283
Hassan Feroze <hferoze@qti.qualcomm.com>
2018-03-05 17:11:45 -0800

    Speech Recognizer for Android
    Master

---
+++ SpeechRecognizer/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/*
+ * Created by hferoze 
+ */
+
+public class GUIManager : MonoBehaviour{
+
+    public void OnStartListeningBtnClick(){
+#if UNITY_ANDROID && !UNITY_EDITOR
+        PluginHelper.StartListening();
+#endif
+    }
+
+    public void Scale(GameObject obj){
+        StartCoroutine(ScaleUpDown(obj));      
+    }
+
+    private IEnumerator ScaleUpDown(GameObject obj){
+        yield return new WaitForEndOfFrame();
+        Vector3 curr_scale = obj.transform.localScale;
+        Vector3 mid_scale = new Vector3(curr_scale.x + curr_scale.x/5f, curr_scale.y + curr_scale.y/5f, curr_scale.z);
+        Vector3 final_scale = curr_scale;
+        float totalT = 0.5f;
+        float currT = 0f;
+
+        while (currT < totalT){
+            currT += Time.deltaTime;
+            Vector3 a = Vector3.Lerp(curr_scale, mid_scale, currT / totalT);
+            Vector3 b = Vector3.Lerp(mid_scale, final_scale, currT / totalT);
+            obj.transform.localScale = Vector3.Lerp(a, b, currT / totalT);
+            yield return null;
+        }
+    }
+}
---
+++ SpeechRecognizer/Assets/Scripts/PluginHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Created by hferoze 
+ */
+
+public class PluginHelper : MonoBehaviour {
+
+	private static string TAG = "Speech-To-Text";
+
+	private static AndroidJavaObject unityPlugin = null;
+	private static AndroidJavaObject activityContext = null;
+
+	private static Dictionary<string, string> inputExtras = new Dictionary<string, string>();
+
+	void Start(){
+#if UNITY_ANDROID && !UNITY_EDITOR
+        if (unityPlugin == null) {
+
+			using (AndroidJavaClass activityClass = new AndroidJavaClass ("com.unity3d.player.UnityPlayer")) {
+				activityContext = activityClass.GetStatic<AndroidJavaObject> ("currentActivity");
+            }
+
+			using (AndroidJavaClass pluginClass = new AndroidJavaClass ("com.hferoze.speechtotextplugin.SpeechToTextUnityBridge")) {
+				if (pluginClass != null) {
+                    unityPlugin = pluginClass.CallStatic<AndroidJavaObject> ("getInstance");
+					unityPlugin.Call ("setContext", activityContext);
+                }
+			}
+		}
+#endif
+    }
+    
+    public static void StartListening(){
+        if (activityContext != null)
+        {
+            activityContext.Call("runOnUiThread", new AndroidJavaRunnable(() => {
+                unityPlugin.Call("getSpeechInput");
+            }));
+        }
+    }
+}
