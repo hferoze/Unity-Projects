5a8c5d27332a2c2dec332d6855b8cd84e28c0949
Hassan Feroze <hferoze@qti.qualcomm.com>
2018-03-05 17:26:32 -0800

    Speech To Text for Android
    Master

---
+++ SpeechRecognizer/Assets/Scripts/SpeechRecogResultUpdate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * Created by hferoze 
+ */
+
+public class SpeechRecogResultUpdate : MonoBehaviour {
+
+    public GameObject SpeechResultTextGO;
+
+    private Text SpeechResultText;
+
+    void Start(){
+        SpeechResultText = SpeechResultTextGO.GetComponent<Text>();
+    }
+
+	public void UpdateResultText(string txt){
+        string[] matches = txt.Split('\n');
+        foreach (string m in matches){
+            Debug.Log("match : " + m);
+        }
+        SpeechResultText.text = txt;
+    }
+}
